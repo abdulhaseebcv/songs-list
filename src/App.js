6a82ac3ccd48fe074f57e0036004ac725ef6f18c
abdulhaseebcv <abdulcvhaseeb@gmail.com>
2023-12-06 18:04:30 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Login from './Components/Login/Login';
 import VerifyLogin from './Components/Login/VerifyLogin';
 import HomePage from './Pages/HomePage';
 import ProtectedRoute from './Components/Login/ProtectedRoute';
 
 function App() {
-   // Checking if a token is present in local storage
-  const token = localStorage.getItem('token')
   return (
     <div className="App">
       <Routes>
